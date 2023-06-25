0137f406a52718a84af30991a1d9cc9ce4feab22
unknown <salimliaynur@gmail.com>
2023-06-26 00:59:51 +0400

    profile is still in progress

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,35 @@
-export default function Profile() {
+import React from "react";
+import { Page, Layout, Card, Text } from "@shopify/polaris";
+import { Stack } from "@shopify/polaris/utilities";
+import "@shopify/polaris/build/esm/styles.css";
+
+export const Profile = () => {
   return (
-    <div>Profile</div>
-  )
+    <Page>
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <Stack vertical>
+              <Text>Name: John Doe</Text>
+              <Text>Email: johndoe@example.com</Text>
+              <Text>Company: ABC Company</Text>
+              <Text>Position: Frontend Developer</Text>
+            </Stack>
+          </Card>
+        </Layout.Section>
+
+        <Layout.Section>
+          <Card sectioned>
+            <Text>Progress of My Objectives</Text>
+            <ul>
+              <li>Objective 1</li>
+              <li>Objective 2</li>
+              <li>Objective 3</li>
+            </ul>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
 }
+
--- src/routing/index.jsx
+++ src/routing/index.jsx
@@ -5,6 +5,8 @@ import { SignUp } from "@/pages/SignUp";
 import { MainLayout } from "@/components/MainLayout";
 import { Organization } from "@/pages/Organization";
 import { NotFound } from "@/pages/NotFound";
+import { Profile } from "@/pages/Profile";
+
 
 export const Routing = () => {
 
@@ -21,6 +23,7 @@ export const Routing = () => {
         <Routes>
             <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<SignUp />} />
+            <Route path='/profile' element = {<Profile/>}/>
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<Organization />} />
             </Route>
