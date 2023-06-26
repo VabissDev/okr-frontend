517a0e04a0618c76faa0cccf45a078b4ff77c096
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-06-26 15:52:09 +0400

    Add SignUp page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,78 @@
+import { useDispatch, useSelector } from "react-redux";
+import { signup } from "../redux/slices/userSlice";
+import { useState } from "react";
+import {
+  Page,
+  Card,
+  Form,
+  TextField,
+  Button,
+  FormLayout,
+} from "@shopify/polaris";
 
 export const SignUp = () => {
+  const dispatch = useDispatch();
+  const { users } = useSelector((state) => state.users);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleNameChange = (value) => {
+    setName(value);
+  };
+
+  const handleEmailChange = (value) => {
+    setEmail(value);
+  };
+
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      signup({
+        id: Date.now(),
+        name,
+        email,
+        password,
+      })
+    );
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
   return (
-    <div>SignUp</div>
-  )
-}
+    <Page title="Sign up">
+      <Card>
+        <Form onSubmit={handleSubmit}>
+          <FormLayout>
+            <TextField
+              label="Full Name"
+              type="text"
+              value={name}
+              onChange={handleNameChange}
+            />
+            <TextField
+              label="Email"
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <Button primary submit>
+              Sign Up
+            </Button>
+          </FormLayout>
+        </Form>
+      </Card>
+    </Page>
+  );
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,6 @@ import userReducer from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    users: userReducer,
   },
 });
