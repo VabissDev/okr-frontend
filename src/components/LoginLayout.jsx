ac1c11d58fa852a1da8ae4d7eb91143367186eed
ZuuZaa <zumbaghirova@gmail.com>
2023-06-26 17:12:32 +0400

    login layout applied

--- src/components/LoginLayout.jsx
+++ src/components/LoginLayout.jsx
@@ -1,14 +1,17 @@
 import { Card, Form, FormLayout, Page, Text } from "@shopify/polaris"
 import { FormWrapper } from "@/styled/containers"
 
-export const LoginLayout = ({ title}) => {
+export const LoginLayout = ({ title, children, onSubmit }) => {
   return (
     <Page>
-        <FormWrapper>
-          <Card>
-            <Text variant="heading2xl" as="h1" children={title}/>
-          </Card>
-        </FormWrapper>
+      <FormWrapper>
+        <Card>
+          <Text variant="heading2xl" as="h1" children={title} />
+          <Form onSubmit={onSubmit}>
+            <FormLayout children={children} />
+          </Form>
+        </Card>
+      </FormWrapper>
     </Page>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,34 @@
-import { Button, Form, FormLayout, TextField } from "@shopify/polaris"
+import { Button, TextField } from "@shopify/polaris"
 import { LoginLayout } from "@/components/LoginLayout"
 
 export const Login = () => {
 
+    const title = "Log in";
+    const onSubmit = () => { console.log("submitted") }
+
     return (
-        <LoginLayout title="Log In">
-            <Form>
-                <FormLayout>
-                    <TextField
-                        type="email"
-                        placeholder="example@site.com"
-                        label="Username / Email:"
-                        onChange={(e) => { console.log(e.target.value); }}
-                        autoComplete="email"
-                    />
-                    <TextField
-                        label="Password:"
-                        type="password"
-                        onChange={(e) => { console.log(e.target.value); }}
-                    />
-                    <Button
-                        submit
-                        fullWidth
-                        primary
-                        children="Log In"
-                    />
-                </FormLayout>
-            </Form>
+        <LoginLayout
+            title={title}
+            onSubmit={onSubmit}
+        >
+            <TextField
+                type="email"
+                placeholder="example@site.com"
+                label="Username / Email:"
+                onChange={(e) => { console.log(e.target.value); }}
+                autoComplete="email"
+            />
+            <TextField
+                label="Password:"
+                type="password"
+                onChange={(e) => { console.log(e.target.value); }}
+            />
+            <Button
+                submit
+                fullWidth
+                primary
+                children="Log In"
+            />
         </LoginLayout>
     )
 }
