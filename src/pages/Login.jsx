e7aca567395b60c5cf816a1b88cb1cdf54924491
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-06-27 18:16:10 +0400

    Add error message to login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,82 +1,73 @@
-import { Button, TextField, Text, Divider, Icon } from "@shopify/polaris"
-import { LoginLayout } from "@/components/LoginLayout"
+import { Button, TextField, Text, Divider, Icon } from "@shopify/polaris";
+import { LoginLayout } from "@/components/LoginLayout";
 import { Link } from "react-router-dom";
-import {
-    ViewMinor,
-    HideMinor
-} from '@shopify/polaris-icons';
+import { ViewMinor, HideMinor } from "@shopify/polaris-icons";
 import { useState } from "react";
 import { PasswordInputWrapper } from "../styled/inputs";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+  const title = "Log in";
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [showPassword, setShowPassword] = useState(false)
-    const title = "Log in";
+  const handleEmailChange = (value) => {
+    setEmail(value);
+  };
 
-    const handleEmailChange = (value) => {
-        setEmail(value);
-    };
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  };
 
-    const handlePasswordChange = (value) => {
-        setPassword(value);
-    };
+  const onSubmit = () => {
+    if (!email.trim()) {
+      setError("Please enter your email address.");
+      return;
+    } else if (!password.trim()) {
+      setError("Please enter a password.");
+      return;
+    } else if (password.trim().length < 8) {
+      setError("Password should be at least 8 characters long.");
+      return;
+    }
 
-    const onSubmit = () => { console.log("submitted") }
+    setError("");
+    console.log("submitted");
+  };
 
-
-
-
-    return (
-        <LoginLayout
-            title={title}
-            onSubmit={onSubmit}
+  return (
+    <LoginLayout title={title} onSubmit={onSubmit}>
+      <TextField
+        type="email"
+        placeholder="example@site.com"
+        label="Username / Email:"
+        value={email}
+        onChange={handleEmailChange}
+        autoComplete="email"
+        error={error && error.includes("email") ? error : ""}
+      />
+      <PasswordInputWrapper>
+        <TextField
+          label="Password:"
+          type={showPassword ? "text" : "password"}
+          placeholder="*********"
+          value={password}
+          onChange={handlePasswordChange}
+          error={error && error.toLowerCase().includes("password") ? error : ""}
+        />
+        <Button
+          className="show-password-btn"
+          onClick={() => setShowPassword(!showPassword)}
         >
-            <TextField
-                type="email"
-                placeholder="example@site.com"
-                label="Username / Email:"
-                value={email}
-                onChange={handleEmailChange}
-                autoComplete="email"
-            />
-            <PasswordInputWrapper>
-                <TextField
-                    label="Password:"
-                    type={showPassword ? "text" : "password"}
-                    placeholder="*********"
-                    value={password}
-                    onChange={
-
-                        handlePasswordChange
-                    }
-                />
-                <Button
-                    className="show-password-btn"
-                    onClick={() => setShowPassword(!showPassword)}
-                >
-                    <Icon
-                        source={showPassword ? ViewMinor : HideMinor}
-                        color="base"
-                    />
-                </Button>
-            </PasswordInputWrapper>
-            <Button
-                submit
-                fullWidth
-                primary
-                children="Log In"
-            />
-            <Divider />
-            <Text
-                alignment="center"
-                variant="headingSm"
-                as="p"
-                color="subdued"
-            >
-                Or  <Link to="/signup"> Sign Up</Link>
-            </Text>
-        </LoginLayout>
-    )
-}
+          <Icon source={showPassword ? ViewMinor : HideMinor} color="base" />
+        </Button>
+      </PasswordInputWrapper>
+      <Button submit fullWidth primary children="Log In" />
+      <Divider />
+      <Text alignment="center" variant="headingSm" as="p" color="subdued">
+        Or <Link to="/signup"> Sign Up</Link>
+      </Text>
+    </LoginLayout>
+  );
+};
