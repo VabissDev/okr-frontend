e85f7f67e85f7dc2ef5225b21fc28850c0a280a0
agasefamircan <agasefemircanov@gmail.com>
2023-06-27 16:36:36 +0400

    login error fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,53 +1,49 @@
-import { Button, TextField, Text, Divider, Icon } from "@shopify/polaris"
-import { LoginLayout } from "@/components/LoginLayout"
+import React, { useState } from "react";
+import { Button, TextField, Text, Divider, Icon } from "@shopify/polaris";
+import { LoginLayout } from "@/components/LoginLayout";
 import { Link } from "react-router-dom";
-import {
-    ViewMajor
-  } from '@shopify/polaris-icons';
+import { ViewMajor } from "@shopify/polaris-icons";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleEmailChange = (value) => {
+    setEmail(value);
+  };
 
-    const title = "Log in";
-    const onSubmit = () => { console.log("submitted") }
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  };
+  const title = "Log in";
+  const onSubmit = () => {
+    setEmail("");
+    setPassword("");
+    console.log("submitted");
+  };
 
-
-    return (
-        <LoginLayout
-            title={title}
-            onSubmit={onSubmit}
-        >
-            <TextField
-                type="email"
-                placeholder="example@site.com"
-                label="Username / Email:"
-                onChange={(e) => { console.log(e.target.value); }}
-                autoComplete="email"
-            />
-            <TextField
-                label="Password:"
-                type="password"
-                placeholder="*********"
-                onChange={(e) => { console.log(e.target.value); }}
-            />
-<Icon
-  source={ViewMajor}
-  color="base"
-/>
-            <Button
-                submit
-                fullWidth
-                primary
-                children="Log In"
-            />
-            <Divider/>
-            <Text
-                alignment="center"
-                variant="headingSm"
-                as="p"
-                color="subdued"
-            >
-                Or  <Link to="/signup"> Sign Up</Link>
-            </Text>
-        </LoginLayout>
-    )
-}
+  return (
+    <LoginLayout title={title} onSubmit={onSubmit}>
+      <TextField
+        type="email"
+        placeholder="example@site.com"
+        label="Username / Email:"
+        value={email}
+        onChange={handleEmailChange}
+        autoComplete="email"
+      />
+      <TextField
+        label="Password:"
+        type="password"
+        placeholder="*********"
+        onChange={handlePasswordChange}
+        value={password}
+      />
+      <Icon source={ViewMajor} color="base" />
+      <Button submit fullWidth primary children="Log In" />
+      <Divider />
+      <Text alignment="center" variant="headingSm" as="p" color="subdued">
+        Or <Link to="/signup"> Sign Up</Link>
+      </Text>
+    </LoginLayout>
+  );
+};
