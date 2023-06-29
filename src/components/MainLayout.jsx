2c5c67dc0c1e0a0708838b2480be4322f6229bf1
aytanbayramova <112419467+aytanbayramova@users.noreply.github.com>
2023-06-29 10:37:30 +0400

    pull request

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,13 +1,50 @@
 import { Page } from '@shopify/polaris'
 import { Outlet } from 'react-router-dom'
 import { Header } from './Header'
+import {Frame, Navigation} from '@shopify/polaris';
+import {HomeMinor} from '@shopify/polaris-icons';
+import React from 'react';
 
 export const MainLayout = () => {
 
   return (
     <Page >
         <Header/>
- 
+       <Frame>
+      <Navigation location="/">
+        <Navigation.Section
+          items={[
+            {
+              url: '',
+              label: 'Workspace Management',
+              icon: HomeMinor,
+            },
+            {
+              url: '/profile/:id',
+              excludePaths: ['/'],
+              label: 'Profile',
+            
+          
+            },
+            {
+              url: '/',
+              excludePaths: ['/'],
+              label: 'Edit Profile',
+            
+          
+            },
+            {
+              url: '/workspace/create',
+              excludePaths: ['/'],
+              label: 'Create Workspace',
+            
+          
+            },
+           
+          ]}
+        />
+      </Navigation>
+    </Frame>
         <Outlet/>
     </Page>
   )
