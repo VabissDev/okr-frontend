bbde65c257867f42daa3c17f61b1b17a8b602811
SananM <smuzefferov1@gmail.com>
2023-06-29 20:54:26 +0400

    updated

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,31 +1,13 @@
-import { Box, Button, FormLayout, HorizontalStack, Icon, Label, Tag, Text, TextField } from "@shopify/polaris";
+import { Button, FormLayout, TextField } from "@shopify/polaris";
 //import { Avatar } from "@shopify/polaris";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { TagWrapper } from "../styled/inputs";
-import { CancelMinor } from "@shopify/polaris-icons";
 
 
-const user = {
-  fullName: "John Doe",
-  email: "john@gmail.com",
-  orgName: "Company #1",
-  password: "********",
-  teams: ["team#1", "team#2", "team#3"]
-}
-
-const removeTag = (option) => console.log(option)
-
-
-
-const EditProfile = ({ onSave }) => {
-
+const EditProfile = ({ user,onSave }) => {
+ 
   //const [selectedImage, setSelectedImage] = useState("");
   const navigate = useNavigate();
-
-
-
-
 
   /*
   const handleImageChange = (event) => {
@@ -43,15 +25,15 @@ const EditProfile = ({ onSave }) => {
     email: user.email,
     password: user.password,
     orgName: user.orgName,
-    teams: user.teamName,
+    teamName: user.teamName,
   });
 
   const handleInputChange = (value, name) => {
-    setProfile((prevProfile) => ({
-      ...prevProfile,
-      [name]: value,
-    }));
-  };
+      setProfile((prevProfile) => ({
+        ...prevProfile,
+        [name]: value,
+      }));
+    };
 
   const handleSaveClick = () => {
     onSave(profile);
@@ -70,14 +52,6 @@ const EditProfile = ({ onSave }) => {
           required
         />
         <TextField
-          label="Organization Name:"
-          type="text"
-          name="orgName"
-          value={profile.orgName}
-          onChange={(value) => handleInputChange(value, "orgName")}
-          readOnly
-        />
-        <TextField
           label="Email:"
           type="email"
           name="email"
@@ -86,31 +60,29 @@ const EditProfile = ({ onSave }) => {
           readOnly
         />
         <TextField
-          label="Reset Your Password:"
+          label="Password:"
           type="password"
           name="password"
           value={profile.password}
           onChange={(value) => handleInputChange(value, "password")}
           required
         />
-
-        <Box>
-          <Label>
-            Teams: 
-          </Label>
-          <HorizontalStack gap="4" >
-          {
-            user.teams.map((option) => (
-              <TagWrapper>
-                <Tag key={option} onRemove={removeTag(option)}>
-                  {option} <Icon source={CancelMinor} color="base" />
-                </Tag>
-              </TagWrapper>
-            ))
-          }
-        </HorizontalStack>
-        </Box>
-
+        <TextField
+          label="Organization Name:"
+          type="text"
+          name="orgName"
+          value={profile.orgName}
+          onChange={(value) => handleInputChange(value, "orgName")}
+          readOnly
+        />
+        <TextField
+          label="Team Name:"
+          type="text"
+          name="teamName"
+          value={profile.teamName}
+          onChange={(value) => handleInputChange(value, "teamName")}
+          readOnly
+        />
         {/*
         <label>Select an image:</label>
         <input type="file"  onChange={handleImageChange} name="image" required />
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Btn, Display } from "../styled/profilee";
 import { Space } from "../styled/profilee";
 import {
@@ -6,40 +7,45 @@ import {
   Text,
   VerticalStack,
   HorizontalStack,
-  Label,
-  Box,
-  Tag,
 } from "@shopify/polaris";
+import { Icon } from "@shopify/polaris";
+import { EditMinor } from "@shopify/polaris-icons";
 import { Link } from "react-router-dom";
 
-
-export const Profile = () => {
-  const user = {
-    fullName: "John Doe",
-    email: "johndoe@gmail.com",
-    password: "**********",
-    org_name: "VABISS",
-    teams: ["team#1", "team#2", "team#3"]
-  };
+export const Profile = ({ user, onSave}) => {
+  
   const id = 1;
+
+  const [updatedUser, setUpdatedUser] = useState(null);
+
+  const handleSaveProfile = (updatedProfile) => {
+    setUpdatedUser(updatedProfile);
+    onSave(updatedProfile);
+  };
+
   return (
     <Card>
+      <Text variant="heading3xl" as="h1">
+        My profile
+      </Text>
       <HorizontalStack>
         <Space>
-          <Box>
-            <Text variant="heading2xl" as="h2">
-              {user.fullName}
-            </Text>
-            <Space>
-              <Text fontWeight="semibold" variant="headingMd" as="p">Organization: </Text>
-              <Text>{user.org_name}</Text>
-            </Space>
-            <Space>
-            <Text fontWeight="semibold" variant="headingMd" as="p">Email: </Text>
-              <Text>{user.email}</Text>
-            </Space>
-          </Box>
-          <img
+          {/*
+          {user.image ? (
+            <img
+              src={user.image}
+              alt=""
+              style={{ width: 100, height: 100, borderRadius: "50%" }}
+            />
+          ) : (
+            <img
+              src="https://srv1.portal.p-cd.net/850p/2022/04/08/177405-1649405499-962966.jpg"
+              alt=""
+              style={{ width: 100, height: 100, borderRadius: "50%" }}
+            />
+          )}
+          */}
+         <img
             src="https://srv1.portal.p-cd.net/850p/2022/04/08/177405-1649405499-962966.jpg"
             alt=""
             style={{ width: 100, height: 100, borderRadius: "50%" }}
@@ -49,29 +55,39 @@ export const Profile = () => {
       </HorizontalStack>
       <Divider />
 
+      <HorizontalStack>
         <Space>
-          <Text variant="headingXl" as="h3">
+          <Text variant="heading2xl" as="h3">
             Personal Information
           </Text>
-          <Link to={`/editprofile/${id}`}><Btn>Edit</Btn></Link>
+          <Link to={`/editprofile/${id}`}>
+            <Btn>Edit</Btn>
+          </Link>
         </Space>
-        <Space>
-        <Text fontWeight="semibold" variant="headingMd" as="p">Bio: </Text>
-              <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis doloremque beatae hic blanditiis ipsa dolor, iure quisquam nesciunt facilis commodi!</Text>
-            </Space>
+      </HorizontalStack>
 
-        <Box>
-          <Text fontWeight="semibold" variant="headingMd" as="p">Teams: </Text>
-          <HorizontalStack gap="4" >
-            {
-              user.teams.map((option) => (
-                  <Tag key={option}>
-                    {option}
-                  </Tag>
-              ))
-            }
-          </HorizontalStack>
-        </Box>
+      <Display>
+        <div>
+          <Text style="strong">Full name:</Text>
+          <p>{updatedUser ? updatedUser.fullName : user.fullName}</p>
+        </div>
+        <div>
+          <Text style="strong">Email Address:</Text>
+          <p>{updatedUser ? updatedUser.email : user.email}</p>
+        </div>
+        <div>
+          <Text style="strong">Password:</Text>
+          <p>{updatedUser ? updatedUser.password : user.password}</p>
+        </div>
+        <div>
+          <Text style="strong">Organization name:</Text>
+          <p>{updatedUser ? updatedUser.orgName : user.orgName}</p>
+        </div>
+        <div>
+          <Text style="strong">Team name:</Text>
+          <p>{updatedUser ? updatedUser.teamName : user.teamName}</p>
+        </div>
+      </Display>
     </Card>
   );
 };
