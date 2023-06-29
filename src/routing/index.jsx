7a0dc27dcc8bcf68c7629b233e5edff2808a0c82
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-06-29 11:55:50 +0400

    Update routing

--- src/routing/index.jsx
+++ src/routing/index.jsx
@@ -1,5 +1,5 @@
-import { Route, Routes, useNavigate } from "react-router-dom"
-import { useEffect } from "react"
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { Login } from "@/pages/Login";
 import { SignUp } from "@/pages/SignUp";
 import { MainLayout } from "@/components/MainLayout";
@@ -8,32 +8,30 @@ import { WorkspaceManagement } from "@/pages/WorkspaceManagement";
 import { Profile } from "@/pages/Profile";
 import EditProfile from "@/pages/EditProfile";
 import { CreateWorkspace } from "@/components/Workspace/CreateWorkspace";
-import {UserList} from "../components/Users/UserList"
+import { UserList } from "../components/Users/UserList";
 
 export const Routing = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    const login = true;
+    if (!login) {
+      navigate("/login");
+    }
+  }, [navigate]);
 
-    useEffect(() => {
-        const login = true;
-        if (!login) {
-            navigate("/login")
-        }
-    }, [navigate])
-
-    return (
-        <Routes>
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path="/" element={<MainLayout />}>
-            <Route path="/users" element={<UserList />}/>
-                <Route index element={<WorkspaceManagement />} />
-                <Route path='profile/:id' element={<Profile/>} />
-                <Route path='editprofile/:id' element={<EditProfile />} />
-                <Route path='workspace/create' element={<CreateWorkspace />} />
-            </Route>
-            <Route path='*' element={<NotFound />} />
-
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<WorkspaceManagement />} />
+        <Route path="users" element={<UserList />} />
+        <Route path="profile/:id" element={<Profile />} />
+        <Route path="editprofile/:id" element={<EditProfile />} />
+        <Route path="workspace/create" element={<CreateWorkspace />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
