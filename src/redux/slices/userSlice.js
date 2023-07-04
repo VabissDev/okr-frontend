c82a80e49f3ea39917ec47f86f17b1c0d814d577
ZuuZaa <zumbaghirova@gmail.com>
2023-07-04 23:40:33 +0400

    login form errors handling

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -3,19 +3,41 @@ import users from '../../data/users.json';
 
 const initialState = {
   users: [...users],
+  login: false,
+  account: {}
+
 };
 
 export const userSlice = createSlice({
-  name: "user",
+  name: "users",
   initialState,
   reducers: {
     signup: (state, action) => {
       state.users.push(action.payload);
     },
-    // login: (state, action) => {},
+    account: {
+      reducer(state, action) {
+          state.account= action.payload;
+      },
+      prepare(id) {
+
+        const account = users.find(user => user.id === id)
+        console.log(account)
+          return {
+              payload: {
+                  id
+              }
+          }
+      }
+  }, 
+    
   },
 });
 
 export const { signup } = userSlice.actions;
+
+export const getAllUsers = state => state.users.users
+export const login = state => state.users.login
+export const account = state => state.users.account
 
 export default userSlice.reducer;
