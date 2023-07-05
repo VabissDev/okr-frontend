fe8f38028ad1babb05478be3fd64b39d18cd8caa
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-07-05 11:32:53 +0400

    Fix two children with the same key issue

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,61 +1,71 @@
 import { Navigation, Text } from "@shopify/polaris";
-import { HomeMinor, HashtagMinor, LockMinor } from '@shopify/polaris-icons';
+import { HomeMinor, HashtagMinor, LockMinor } from "@shopify/polaris-icons";
 import { useSelector } from "react-redux";
 import { getAllWorkspaces } from "../redux/slices/workspaceSlices";
 import { getAccountData } from "../redux/slices/accountSlice";
 
-
 export const Navigations = () => {
-
-
-  const workspaces = useSelector(getAllWorkspaces)
+  const workspaces = useSelector(getAllWorkspaces);
   const account = useSelector(getAccountData);
 
-  const navlinks = [{
-    label: <Text variant="headingXl" as="h3" color="subdued" children={account.org_name} /> ,
-    icon: HomeMinor,
-  }]
+  const navlinks = [
+    {
+      key: account.id,
+      label: (
+        <Text
+          variant="headingXl"
+          as="h3"
+          color="subdued"
+          children={account.org_name}
+        />
+      ),
+      icon: HomeMinor,
+    },
+  ];
 
-  const filtered = workspaces.filter(workspace => workspace.org_name === account.org_name);
+  const filtered = workspaces.filter(
+    (workspace) => workspace.org_name === account.org_name
+  );
 
-  filtered.map(item => {
-
+  filtered.map((item) => {
     const icon = item.visibility === "public" ? HashtagMinor : LockMinor;
-    const label = <Text variant="headingMd" as="h4" color="subdued" children={item.name} />
+    const label = (
+      <Text variant="headingMd" as="h4" color="subdued" children={item.name} />
+    );
     navlinks.push({
+      key: item.id,
       url: `/workspace/${item.id}`,
       label,
-      icon
-    })
-  })
-
+      icon,
+    });
+  });
 
   return (
     <Navigation location="/">
       <Navigation.Section
         items={navlinks}
-      // items={[
-      //   {
-      //     url: "/",
-      //     label: "Workspace Management",
-      //     icon: HomeMinor,
-      //   },
-      //   {
-      //     url: "/profile/:id",
-      //     excludePaths: ["/"],
-      //     label: "Profile",
-      //   },
-      //   {
-      //     url: "/editprofile/:id",
-      //     excludePaths: ["/"],
-      //     label: "Edit Profile",
-      //   },
-      //   {
-      //     url: "/workspace/create",
-      //     excludePaths: ["/"],
-      //     label: "Create Workspace",
-      //   },
-      // ]}
+        // items={[
+        //   {
+        //     url: "/",
+        //     label: "Workspace Management",
+        //     icon: HomeMinor,
+        //   },
+        //   {
+        //     url: "/profile/:id",
+        //     excludePaths: ["/"],
+        //     label: "Profile",
+        //   },
+        //   {
+        //     url: "/editprofile/:id",
+        //     excludePaths: ["/"],
+        //     label: "Edit Profile",
+        //   },
+        //   {
+        //     url: "/workspace/create",
+        //     excludePaths: ["/"],
+        //     label: "Create Workspace",
+        //   },
+        // ]}
       />
     </Navigation>
   );
