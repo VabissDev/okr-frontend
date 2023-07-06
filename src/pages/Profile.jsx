ceb6b733c4decc43d8d9a1e747e5f09b4a1d136e
unknown <salimliaynur@gmail.com>
2023-07-06 12:48:17 +0400

    Profile edited with buttons

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -55,7 +55,7 @@ export const Profile = ({ istifadeci, onSave }) => {
         <img
           src={avatarSource}
           alt=""
-          style={{ width: 100, height: 100, borderRadius: "50%" }}
+          style={{ width: 99, height: 99, borderRadius: "50%" }}
         />
       </Space>
       <Divider />
