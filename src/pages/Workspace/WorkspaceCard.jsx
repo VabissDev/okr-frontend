b8e9c41cb9294c44f80a01fa6a98233c3c40f7fe
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-07-06 15:02:10 +0400

    Implement random illustrator for workspaces

--- src/pages/Workspace/WorkspaceCard.jsx
+++ src/pages/Workspace/WorkspaceCard.jsx
@@ -1,11 +1,11 @@
 import { CalloutCard } from "@shopify/polaris";
 import React from "react";
 
-export const WorkspaceCard = ({ name, description, id }) => {
+export const WorkspaceCard = ({ name, description, id, illustration }) => {
   return (
     <CalloutCard
       title={name}
-      illustration="https://ouch-cdn2.icons8.com/_H48roWoztxdlfgJZQRrX_touEv1st371o7fctWmYmA/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNTg2/L2MxOWUxMzFjLTNk/NjUtNDI0OS04NWUz/LTI0MzNlYWFhZjU2/YS5wbmc.png"
+      illustration={illustration}
       primaryAction={{
         content: "Learn More",
         url: `/workspace/${id}`,
