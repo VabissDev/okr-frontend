a3ed209de254ebce57d8a14621ae12de3b4e865a
ZuuZaa <zumbaghirova@gmail.com>
2023-07-08 11:51:11 +0400

    users details bug fixed in profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -32,90 +32,87 @@ export const Profile = ({ istifadeci, onSave }) => {
   const isAdmin = users.some((user) => account.id === profile?.id);
   console.log(profile)
 
-  const ProfileCard = <Card>
-    <Space>
-      <Box>
-        <Text variant="heading3xl" as="h1">
-          {profile?.name}
-        </Text>
-        <Space>
-          <Text variant="headingMd" as="p" fontWeight="semibold">
-            Organization:
-          </Text>
-          <Text>{profile?.org_name}</Text>
-        </Space>
-
-        <Space>
-          <Text variant="headingMd" as="p" fontWeight="semibold">
-            Email:
-          </Text>
-          <Text>{profile?.email}</Text>
-        </Space>
-      </Box>
-      <img
-        src="https://srv1.portal.p-cd.net/850p/2022/04/08/177405-1649405499-962966.jpg"
-        alt=""
-        style={{ width: 100, height: 100, borderRadius: "50%" }}
-      />
-    </Space>
-    <Divider />
-
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        paddingTop: "10px",
-      }}
-    >
-      <div>
-        <Text variant="heading2xl" as="h3">
-          Personal Information
-        </Text>
-      </div>
-      <div>
-        <Link to={`/editprofile/${id}`}>
-          {isAdmin ? (
-            <Button primary>
-              <Icon source={EditMinor} color="base" />
-            </Button>
-          ) : (
-            <p></p>
-          )}
-        </Link>
-      </div>
-    </div>
-
-    <VerticalStack spacing="extraTight">
-      <Top>
-        <Space>
-
-
-          <Text style={{ fontWeight: "bold" }}>Team name:</Text>
-          {profile?.teams.map((team, i) => {
-            return <Tag key={i}>{team}</Tag>;
-          })}
-        </Space>
-      </Top>
-    </VerticalStack>
-    <PageActions
-      primaryAction={{
-        content: "Save",
-      }}
-      secondaryActions={[
-        {
-          content: "Delete",
-          destructive: true,
-        },
-      ]}
-    />
-  </Card>
-
-
   return (
     <>
       {
         profile
-          ? <Card />
+          ?  <Card>
+          <Space>
+            <Box>
+              <Text variant="heading3xl" as="h1">
+                {profile?.name}
+              </Text>
+              <Space>
+                <Text variant="headingMd" as="p" fontWeight="semibold">
+                  Organization:
+                </Text>
+                <Text>{profile?.org_name}</Text>
+              </Space>
+      
+              <Space>
+                <Text variant="headingMd" as="p" fontWeight="semibold">
+                  Email:
+                </Text>
+                <Text>{profile?.email}</Text>
+              </Space>
+            </Box>
+            <img
+              src="https://srv1.portal.p-cd.net/850p/2022/04/08/177405-1649405499-962966.jpg"
+              alt=""
+              style={{ width: 100, height: 100, borderRadius: "50%" }}
+            />
+          </Space>
+          <Divider />
+      
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              paddingTop: "10px",
+            }}
+          >
+            <div>
+              <Text variant="heading2xl" as="h3">
+                Personal Information
+              </Text>
+            </div>
+            <div>
+              <Link to={`/editprofile/${id}`}>
+                {isAdmin ? (
+                  <Button primary>
+                    <Icon source={EditMinor} color="base" />
+                  </Button>
+                ) : (
+                  <p></p>
+                )}
+              </Link>
+            </div>
+          </div>
+      
+          <VerticalStack spacing="extraTight">
+            <Top>
+              <Space>
+      
+      
+                <Text style={{ fontWeight: "bold" }}>Team name:</Text>
+                {profile?.teams.map((team, i) => {
+                  return <Tag key={i}>{team}</Tag>;
+                })}
+              </Space>
+            </Top>
+          </VerticalStack>
+          <PageActions
+            primaryAction={{
+              content: "Save",
+            }}
+            secondaryActions={[
+              {
+                content: "Delete",
+                destructive: true,
+              },
+            ]}
+          />
+        </Card>
           : <EmptyData
             heading="Sorry, user cannot be found."
             actionTitle="Back To Workspaces"
